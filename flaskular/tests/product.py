802be66e993fcc0b6a68696f1776fa43a1889dba
Philipp Spinnler <philipp@spinnler.ch>
2016-08-29 21:10:53 +0200

    Some test ajustments

--- flaskular/tests/product.py
+++ flaskular/tests/product.py
@@ -15,6 +15,12 @@ class ProductTestCase(BaseTestCase):
         assert j['cnetID'] == 'S8725865'
         return j
 
+    def test_products_by_category(self):
+        rv = self.client.get('/products?category=HH_Druckerpatronen_Toner')
+        j = self.parseJsonResponse(rv)
+        assert len(j['products']) > 0
+        return j
+
     def test_product_invalid_code(self):
         rv = self.client.get('/products/not_valid_code')
         assert rv.status_code == 404
--- flaskular/tests/product_category.py
+++ flaskular/tests/product_category.py
@@ -12,7 +12,7 @@ class ProductCategoryTestCase(BaseTestCase):
     def test_productcategories_id(self):
         rv = self.client.get('/productcategories/HH_Druckerpatronen_Toner')
         j = self.parseJsonResponse(rv)
-        assert len(j['products']) > 0
+        assert j['name'] == 'Patronen/Toner'
         return j
 
     def test_productcategories_invalid_id(self):
